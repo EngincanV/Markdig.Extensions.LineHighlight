2e537c6d9dfeb143a0a577c9871399aacc4bafb9
EngincanV <enginveske@gmail.com>
2021-06-24 20:56:45 +0300

    Add highlight code line renderer and extension

---
+++ src/Markdig.Extensions.LineHighlight/Markdown/Extensions/HighlightedCodeBlockExtension.cs
@@ -0,0 +1,38 @@
+﻿using Markdig.Extensions.LineHighlight.Markdown.Renderers;
+using Markdig.Renderers;
+using Markdig.Renderers.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdig.Extensions.LineHighlight.Markdown.Extensions
+{
+    public class HighlightedCodeBlockExtension : IMarkdownExtension
+    {
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+        }
+
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+            if(renderer == null)
+            {
+                throw new ArgumentNullException(nameof(renderer));
+            }
+
+            if(renderer is TextRendererBase<HtmlRenderer> htmlRenderer)
+            {
+                var codeBlockRenderer = htmlRenderer.ObjectRenderers.FindExact<CodeBlockRenderer>();
+            
+                if(codeBlockRenderer != null)
+                {
+                    htmlRenderer.ObjectRenderers.Remove(codeBlockRenderer);
+                }
+
+                htmlRenderer.ObjectRenderers.AddIfNotAlready(new HighlightedCodeBlockRenderer());
+            }
+        }
+    }
+}
